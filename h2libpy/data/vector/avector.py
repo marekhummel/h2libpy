570ff371807e23fb1e0dbd9e09764dff268d2ee0
Marek Hummel <marek.hummel3@gmail.com>
2020-07-21 13:17:33 +0200

    Renamed lib in avector

--- h2libpy/data/vector/avector.py
+++ h2libpy/data/vector/avector.py
@@ -1,4 +1,4 @@
-import h2libpy.lib.avector as lib
+import h2libpy.lib.avector as libavector
 from ctypes import c_double, POINTER
 from h2libpy.base.structwrapper import StructWrapper
 
@@ -7,20 +7,20 @@ class AVector(StructWrapper):
     # ***** Constructors / destructor *****
 
     def __init__(self, cobj):
-        assert(isinstance(cobj, POINTER(lib.LibAVector)))
+        assert(isinstance(cobj, POINTER(libavector.LibAVector)))
         self._as_parameter_ = cobj
 
     def __del__(self):
-        lib.del_avector(self)
+        libavector.del_avector(self)
 
     @classmethod
     def new(cls, dim: int):
-        return cls(lib.new_avector(dim))
+        return cls(libavector.new_avector(dim))
 
     @classmethod
-    def from_subvector(cls, src: 'lib.LibAVector', dim: int, off: int):
-        v = lib.new_avector(dim)
-        lib.init_sub_avector(v, src, dim, off)
+    def from_subvector(cls, src: 'libavector.LibAVector', dim: int, off: int):
+        v = libavector.new_avector(dim)
+        libavector.init_sub_avector(v, src, dim, off)
         return cls(v)
 
     # ***** Properties *****
@@ -34,10 +34,10 @@ class AVector(StructWrapper):
     # ***** Methods ******
 
     def fill(self, value):
-        lib.fill_avector(self, c_double(value))
+        libavector.fill_avector(self, c_double(value))
 
     def rand(self):
-        lib.random_avector(self)
+        libavector.random_avector(self)
 
     def norm(self):
-        return lib.norm2_avector(self)
+        return libavector.norm2_avector(self)
