0055d14f81895a012cac90e61706573a830577d2
Marek Hummel <marek.hummel3@gmail.com>
2020-08-07 14:27:52 +0200

    Added methods for sparsepattern and patentry wrappers

---
+++ h2libpy/data/matrix/patentry.py
@@ -0,0 +1,17 @@
+import h2libpy.lib.sparsepattern as libsparsepattern
+from h2libpy.base.structwrapper import StructWrapper
+from h2libpy.base.util import try_wrap
+
+
+class PatEntry(StructWrapper, cstruct=libsparsepattern.CStructPatEntry):
+    
+    # ***** Properties *****
+
+    def __getter_row(self) -> int:
+        return self.cobj().row
+
+    def __getter_col(self) -> int:
+        return self.cobj().col
+
+    def __getter_next(self) -> 'PatEntry':
+        return try_wrap(self.cobj().next, PatEntry)
--- h2libpy/data/matrix/sparsepattern.py
+++ h2libpy/data/matrix/sparsepattern.py
@@ -4,4 +4,30 @@ from h2libpy.base.structwrapper import StructWrapper
 
 class SparsePattern(StructWrapper,
                     cstruct=libsparsepattern.CStructSparsePattern):
-    pass
+    # ***** Constructors / destructor *****
+
+    @classmethod
+    def new(cls, rows: int, cols: int):
+        return cls(libsparsepattern.new_sparsepattern(rows, cols))
+    
+    # ***** Properties *****
+
+    def __getter_rows(self) -> int:
+        return self.cobj().rows
+
+    def __getter_cols(self) -> int:
+        return self.cobj().cols
+
+    # def __getter_row(self):
+    #     pass
+
+    # ***** Methods ******
+
+    def clear(self):
+        libsparsepattern.clear_sparsepattern(self)
+        
+    def add_nz(self, row: int, col: int):
+        libsparsepattern.addnz_sparsepattern(self, row, col)
+        
+    def print(self):
+        libsparsepattern.print_sparsepattern(self)
