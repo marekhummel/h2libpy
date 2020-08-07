a0dc7a50aef8350d0401752d4c5bde098a7a6330
Marek Hummel <marek.hummel3@gmail.com>
2020-08-07 14:15:17 +0200

    Added methods to krylovsolvers wrapper

--- h2libpy/data/matrix/__init__.py
+++ h2libpy/data/matrix/__init__.py
@@ -1,6 +1,7 @@
 ''' Import classes here to make them available in h2libpy.data.matrix '''
 
 from h2libpy.data.matrix.amatrix import AMatrix
+from h2libpy.data.matrix.dh2matrix import DH2Matrix
 from h2libpy.data.matrix.h2matrix import H2Matrix
 from h2libpy.data.matrix.hmatrix import HMatrix
 from h2libpy.data.matrix.rkmatrix import RkMatrix
--- h2libpy/solver/krylovsolvers.py
+++ h2libpy/solver/krylovsolvers.py
@@ -1,8 +1,120 @@
 import h2libpy.lib.krylovsolvers as libkrylovsolvers
-from h2libpy.data.matrix.amatrix import AMatrix
-from h2libpy.data.vector.avector import AVector
+import h2libpy.lib.krylov as libkrylov
+import h2libpy.data.matrix as mat
+import h2libpy.data.vector as vec
+from h2libpy.base.util import verify_type
+from typing import Union
+from ctypes import cast, c_void_p
 
 
-def solve_cg_amatrix_avector(a: 'AMatrix', b: 'AVector', x: 'AVector',
-                             eps: float, maxiter: int) -> int:
-    return libkrylovsolvers.solve_cg_amatrix_avector(a, b, x, eps, maxiter)
+matrix_types = [mat.AMatrix, mat.SparseMatrix, mat.HMatrix, mat.H2Matrix, 
+                mat.DH2Matrix]
+matrix_union = Union[*matrix_types]
+
+
+def solve_cg_avector(a, addeval, b: 'vec.AVector', x: 'vec.AVector', 
+                     eps: float, maxiter: int) -> int:
+    ca = cast(a, c_void_p)
+    caddeval = libkrylov.CFuncAddevalT(addeval)
+    libkrylovsolvers.solve_cg_avector(ca, caddeval, b, x, eps, maxiter)
+
+
+def solve_cg(a: matrix_union, b: 'vec.AVector', x: 'vec.AVector',
+             eps: float, maxiter: int) -> int:
+    verify_type(a, matrix_types)
+
+    if isinstance(a, mat.AMatrix):
+        solve_func = libkrylovsolvers.solve_cg_amatrix_avector
+    elif isinstance(a, mat.SparseMatrix):
+        solve_func = libkrylovsolvers.solve_cg_sparsematrix_avector
+    elif isinstance(a, mat.HMatrix):
+        solve_func = libkrylovsolvers.solve_cg_hmatrix_avector
+    elif isinstance(a, mat.H2Matrix):
+        solve_func = libkrylovsolvers.solve_cg_h2matrix_avector
+    elif isinstance(a, mat.DH2Matrix):
+        solve_func = libkrylovsolvers.solve_cg_dh2matrix_avector
+    return solve_func(a, b, x, eps, maxiter)
+
+
+def solve_pcg_avector(a, addeval, prcd, pdata, b: 'vec.AVector', 
+                      x: 'vec.AVector', eps: float, maxiter: int) -> int:
+    ca = cast(a, c_void_p)
+    caddeval = libkrylov.CFuncAddevalT(addeval)
+    cprcd = libkrylov.CFuncPrcdT(prcd)
+    cpdata = cast(pdata, c_void_p)
+    libkrylovsolvers.solve_pcg_avector(ca, caddeval, cprcd, cpdata, b, x, eps, 
+                                       maxiter)
+
+
+def solve_pcg(a: matrix_union, prcd, pdata, b: 'vec.AVector', x: 'vec.AVector',
+              eps: float, maxiter: int) -> int:
+    verify_type(a, matrix_types)
+    cprcd = libkrylov.CFuncPrcdT(prcd)
+    cpdata = cast(pdata, c_void_p)
+
+    if isinstance(a, mat.AMatrix):
+        solve_func = libkrylovsolvers.solve_pcg_amatrix_avector
+    elif isinstance(a, mat.SparseMatrix):
+        solve_func = libkrylovsolvers.solve_pcg_sparsematrix_avector
+    elif isinstance(a, mat.HMatrix):
+        solve_func = libkrylovsolvers.solve_pcg_hmatrix_avector
+    elif isinstance(a, mat.H2Matrix):
+        solve_func = libkrylovsolvers.solve_pcg_h2matrix_avector
+    elif isinstance(a, mat.DH2Matrix):
+        solve_func = libkrylovsolvers.solve_pcg_dh2matrix_avector
+    return solve_func(a, cprcd, cpdata, b, x, eps, maxiter)
+
+
+def solve_gmres_avector(a, addeval, b: 'vec.AVector', x: 'vec.AVector', 
+                     eps: float, maxiter: int, kmax: int) -> int:
+    ca = cast(a, c_void_p)
+    caddeval = libkrylov.CFuncAddevalT(addeval)
+    libkrylovsolvers.solve_gmres_avector(ca, caddeval, b, x, eps, maxiter, 
+                                         kmax)
+
+
+def solve_gmres(a: matrix_union, b: 'vec.AVector', x: 'vec.AVector',
+               eps: float, maxiter: int, kmax: int) -> int:
+    verify_type(a, matrix_types)
+
+    if isinstance(a, mat.AMatrix):
+        solve_func = libkrylovsolvers.solve_gmres_amatrix_avector
+    elif isinstance(a, mat.SparseMatrix):
+        solve_func = libkrylovsolvers.solve_gmres_sparsematrix_avector
+    elif isinstance(a, mat.HMatrix):
+        solve_func = libkrylovsolvers.solve_gmres_hmatrix_avector
+    elif isinstance(a, mat.H2Matrix):
+        solve_func = libkrylovsolvers.solve_gmres_h2matrix_avector
+    elif isinstance(a, mat.DH2Matrix):
+        solve_func = libkrylovsolvers.solve_gmres_dh2matrix_avector
+    return solve_func(a, b, x, eps, maxiter, kmax)
+
+
+def solve_pcg_avector(a, addeval, prcd, pdata, b: 'vec.AVector', 
+                      x: 'vec.AVector', eps: float, maxiter: int,
+                      kmax: int) -> int:
+    ca = cast(a, c_void_p)
+    caddeval = libkrylov.CFuncAddevalT(addeval)
+    cprcd = libkrylov.CFuncPrcdT(prcd)
+    cpdata = cast(pdata, c_void_p)
+    libkrylovsolvers.solve_pgmres_avector(ca, caddeval, cprcd, cpdata, b, x, 
+                                          eps, maxiter, kmax)
+
+
+def solve_pgmres(a: matrix_union, prcd, pdata, b: 'vec.AVector', 
+                 x: 'vec.AVector', eps: float, maxiter: int, kmax: int) -> int:
+    verify_type(a, matrix_types)
+    cprcd = libkrylov.CFuncPrcdT(prcd)
+    cpdata = cast(pdata, c_void_p)
+
+    if isinstance(a, mat.AMatrix):
+        solve_func = libkrylovsolvers.solve_pgmres_amatrix_avector
+    elif isinstance(a, mat.SparseMatrix):
+        solve_func = libkrylovsolvers.solve_pgmres_sparsematrix_avector
+    elif isinstance(a, mat.HMatrix):
+        solve_func = libkrylovsolvers.solve_pgmres_hmatrix_avector
+    elif isinstance(a, mat.H2Matrix):
+        solve_func = libkrylovsolvers.solve_pgmres_h2matrix_avector
+    elif isinstance(a, mat.DH2Matrix):
+        solve_func = libkrylovsolvers.solve_pgmres_dh2matrix_avector
+    return solve_func(a, cprcd, cpdata, b, x, eps, maxiter, kmax)
