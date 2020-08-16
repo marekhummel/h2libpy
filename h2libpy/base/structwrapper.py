aed1cd8cd608c2616e38723e57621b3bdd418de0
Marek Hummel <marek.hummel3@gmail.com>
2020-08-16 17:17:10 +0200

    Beautified structwrapper

--- h2libpy/base/structwrapper.py
+++ h2libpy/base/structwrapper.py
@@ -21,22 +21,21 @@ class StructWrapper():
         '''
         # C struct doesnt have this field
         if name not in self.avail_fields():
-            raise AttributeError(f'\'{self.__class__.__name__}\' '
-                                 f'has no attribute \'{name}\'')
+            error = f"'{self.__class__.__name__}' has no attribute '{name}'"
+            raise AttributeError(error)
 
         # Corresponding method for field is not implemented in this class
         getter = f'_{self.__class__.__name__}__getter_{name}'
         if getter not in dir(self.__class__):
-            raise AttributeError(f'\'{self.__class__.__name__}\' '
-                                 f'has no getter for attribute \'{name}\'')
+            raise AttributeError(f"'{self.__class__.__name__}' "
+                                 f"has no getter for attribute '{name}'")
 
         # Return value
         return getattr(self, getter)()
 
-    def cobj(self, no_ptr: bool = False):
-        return deref(self._as_parameter_) \
-            if not no_ptr \
-            else self._as_parameter_
+    def cobj(self):
+        ''' Returns wrapped c object '''
+        return deref(self._as_parameter_)
 
     def avail_fields(self):
         ''' Lists all available fields in c struct '''
