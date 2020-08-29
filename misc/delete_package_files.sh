ba997ae4cfe0fed37316713e11fceb66e3b07048
Marek Hummel <marek.hummel3@gmail.com>
2020-08-29 16:32:51 +0200

    Updated util sh scripts

---
+++ misc/delete_package_files.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -r build/
+rm -r dist/
+rm -r h2libpy.egg-info/
--- misc/delete_pyc.sh
+++ misc/delete_pyc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find ./.. -name '*.pyc' -delete
-find ./.. -name '__pycache__' -delete
+find . -name '*.pyc' -delete
+find . -name '__pycache__' -delete
