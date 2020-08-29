9c06082c1e6a71c68194d5c63872a0c8fa217875
Marek Hummel <marek.hummel3@gmail.com>
2020-08-29 18:48:09 +0200

    Removed pypi token

--- misc/packaging.sh
+++ misc/packaging.sh
@@ -12,6 +12,3 @@ pipenv run python setup.py sdist bdist_wheel
 # Upload to pypi
 # pipenv run python -m twine upload --repository testpypi dist/*
 # pipenv run python -m twine upload dist/*
-
-# Token:
-# pypi-AgENdGVzdC5weXBpLm9yZwIkNzJlNTlkOGUtNWQ2MC00ODJmLThkZTQtNDc1M2MzYTE4NGIwAAIleyJwZXJtaXNzaW9ucyI6ICJ1c2VyIiwgInZlcnNpb24iOiAxfQAABiA-v-oeVfyICYm_jY_6eAMiAT9ydFykqUuyRvSQ91tMqg
