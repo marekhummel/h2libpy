915b00de84951943bfbb0c4494da392b3a92b663
Marek Hummel <marek.hummel3@gmail.com>
2020-09-08 12:15:00 +0200

    Updated benchmark routine

--- benchmark.py
+++ benchmark.py
@@ -1,11 +1,13 @@
 import os
 import sys
 import time
+import datetime as dt
 
 import examples.example_amatrix_bem3d as exa
 import examples.example_h2matrix_bem3d as exht
 import examples.example_hmatrix_bem3d as exh
 
+# print(__file__)
 
 class HiddenPrints:
     def __enter__(self):
@@ -16,20 +18,22 @@ class HiddenPrints:
         sys.stdout.close()
         sys.stdout = self._original_stdout
 
-
-funcs = {'amatrix': exa.main, 'hmatrix': exh.main, 'h2matrix': exht.main}
+# funcs = {'amatrix': exa.main, 'hmatrix': exh.main, 'h2matrix': exht.main}
+funcs = {'hmatrix': exh.main}
 reps = 5
 
 with open('benchmark.txt', 'w') as f:
     for func in funcs.items():
-        for split in [8, 16, 32]:
-            for qreg in [2, 4]:
-                if split == 32 and qreg == 4: break
+        for split in [8]:
+            for qreg in [2]:
+                print(f'{func[0]}: {split},{qreg}')
                 start = time.time()
-                with HiddenPrints():
-                    for _ in range(reps):
-                        norm = func[1](qreg, split)
+                for i in range(reps):
+                    print(f'Attempt {i+1}: {dt.datetime.now().strftime("%H:%M:%S")}')
+                    with HiddenPrints():
+                        norm = func[1](qreg, split)          
                 t = (time.time() - start) / reps
                 f.write(f'{func[0].ljust(8)}: {split:-2d},{qreg} = {t:9.5f}s ({norm})\n')
-                print(f'{func[0]}: {split},{qreg}')
+                f.flush()
+        
         f.write('\n')
