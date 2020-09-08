b98af8d2350b28a46cb326ac082b428d7cbaf8ad
Marek Hummel <marek.hummel3@gmail.com>
2020-09-08 12:16:32 +0200

    Fixed bug in library loading

--- h2libpy/__init__.py
+++ h2libpy/__init__.py
@@ -1,9 +1,13 @@
 import ctypes
+import os
 from ctypes import POINTER as PTR
 from ctypes import c_char, c_int
 
+# Root folder of this module
+root = os.path.join(os.path.dirname(__file__), '..')
+
 # Init h2lib and load library instance
-lib = ctypes.CDLL('../H2Lib/libh2.so')  # './lib/libh2.so'
+lib = ctypes.CDLL(os.path.join(root, 'h2libpy/libh2.so'))
 init_h2lib = lib.init_h2lib
 init_h2lib.restype = None
 init_h2lib.argtypes = [PTR(c_int), PTR(PTR(PTR(c_char)))]
--- setup.py
+++ setup.py
@@ -14,4 +14,6 @@ setup(name='h2libpy',
       author_email='marek.hummel3@gmail.com',
       license='LGPL-3.0',
       packages=find_packages(exclude=['examples']),
+      package_data={'h2libpy': ['libh2.so']},
+      include_package_data=True,
       python_requires='>=3.8',)
